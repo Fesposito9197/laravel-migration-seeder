761fc8c05bceca1bdf2fc46fef15708a6043f166
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-17 15:06:51 +0100

    aggiunge creazione tabella

--- database/migrations/2023_01_17_133620_create_trains_table.php
+++ database/migrations/2023_01_17_133620_create_trains_table.php
@@ -15,8 +15,21 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company' , 100);
+            $table->string('from', 100);
+            $table->string('to', 100);
+            $table->dateTime('time_of_departure',$precision = 0);
+            $table->dateTime('time_of_arrival', $precision = 0);
+            $table->string('lenght_of_journey', 20);
+            $table->unsignedTinyInteger('stops');
+            $table->string('train_number', 30);
+            $table->string('train_carriage', 30);
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('cancelled')->default(1);
             $table->timestamps();
         });
+
+
     }
 
     /**

27270515f748d5f4ac4254efbd96b6193e87197d
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-17 15:26:17 +0100

    aggiunge stampa dei treni con partenza oggi

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        // $trains = Train::all(); //stampo tutta la lista dei treni
+        
+        $trains = Train::whereDate('time_of_departure','2023-01-17')->get();
+
+        return view('welcome', compact('trains'));
+
+    }
 }
